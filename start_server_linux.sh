63156c69b21b4872ccd1ff5f67f1b8a96622355f
lindsey98 <kelseyliu1998@gmail.com>
2022-04-11 14:18:57 +0800

    change start_server scripts

--- start_server_linux.sh
+++ start_server_linux.sh
@@ -1,2 +1,1 @@
-cd /home/ruofan/git_space/MyScrapy/
 python server.py & python catch_phishing.py
--- start_server_windows.bat
+++ start_server_windows.bat
@@ -1,5 +1,5 @@
 @echo off
 
-start cmd /k "cd /home/ruofan/git_space/MyScrapy & python server.py"
-start cmd /k "cd /home/ruofan/git_space/MyScrapy & python catch_phishing.py"
+start cmd /k "python server.py"
+start cmd /k "python catch_phishing.py"
 
