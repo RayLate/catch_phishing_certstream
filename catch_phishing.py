24b14c6ad53e5efbc6358da499749c266da52100
lindsey98 <51521323+lindsey98@users.noreply.github.com>
2022-07-19 14:15:32 +0800

    Update catch_phishing.py

--- catch_phishing.py
+++ catch_phishing.py
@@ -19,28 +19,48 @@ if __name__ == '__main__':
     PORT = 8889
     connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     connection.connect((SERVER, PORT))
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--feed_save_path', default="D:/ruofan/phishing-kit-database/openphish/feed_download")
+    parser.add_argument('--save_dir', default="D:/ruofan/New_phish30k")
+    args = parser.parse_args()
 
     print("connected")
-    log_suspicious = "./company_list.txt"
+#     log_suspicious = "./company_list.txt"
+#     with open('./brand_list.json', 'r', encoding='utf-8') as f:
+#         brand_list = json.load(f)
+#     print("total number of brands: {}".format(len(list(brand_list.keys()))))
 
-    with open('./brand_list.json', 'r', encoding='utf-8') as f:
-        brand_list = json.load(f)
-    print("total number of brands: {}".format(len(list(brand_list.keys()))))
+    c = boto3.client(
+        's3',
+        aws_access_key_id="",
+        aws_secret_access_key="")
+    
+    with open("{}/feed30day_".format(args.feed_save_path)+time_now+'.tar.gz', 'wb') as local_feed:
+        c.download_fileobj('opfeeds', "premium_plus/30_days_archive.tar.gz", local_feed)
+        # open file
+        file = tarfile.open("{}/feed30day_".format(args.feed_save_path)+time_now+'.tar.gz')]
+        os.makedirs("{}/feed30day_".format(args.feed_save_path)+time_now, exist_ok=True)
+        file.extractall("{}/feed30day_".format(args.feed_save_path)+time_now)
+        file.close()
+        
+    with open("{}/feed30day_".format(args.feed_save_path)+time_now+'/premium_phishing_feed_30_days.json', "rb") as local_feed:
+        feed_today = json.load(local_feed) # print("Number of phishing {}".format(len(feed_today)))
 
-    for brand in list(brand_list.keys())[int(len(list(brand_list.keys()))//3):int(len(list(brand_list.keys()))//3)*2]: # FIXME: replace with your own URL list
+        
+    for feed in feed_today: 
         pbar.update(1)
-        domain = brand_list[brand]["url"]
+        domain = clean_domain(feed["url"].split('://')[1].split('/')[0], '\/:*?"<>|')
+        dirname = os.path.join(args.save_dir, domain)
+        
+        if os.path.exists(dirname):
+            continue
+        os.makedirs(dirname)
+        with open(os.path.join(dirname, 'info.txt'), 'w', encoding='utf-8') as fw:
+            fw.write(str(feed))
+        
         try:
             connection.send(domain.encode('utf-8'))
-
-            if os.path.exists(log_suspicious):
-                existing = set(filter(None, set(open(log_suspicious,'r').read().split('\n'))))
-                with open(log_suspicious, 'a') as f:
-                    if domain not in existing:
-                        f.write("{}\n".format(domain))
-            else:
-                with open(log_suspicious, 'a') as f:
-                    f.write("{}\n".format(domain))
         except:
             continue
 
