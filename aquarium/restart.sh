d6ac84622248064fac4f3604e44446a62d5d7b77
kelsey <kelseyliu1998@gmail.com>
2023-02-28 13:29:22 +0800

    docker restart

--- aquarium/restart.sh
+++ aquarium/restart.sh
@@ -1,7 +1,10 @@
-#!/bin/sh
-cd /home/qing/aquarium
-
-for index in 0 1 2 3 4
-do 
-	sudo docker-compose restart splash$index
+cd ~/aquarium/
+sudo service docker restart
+while true
+do
+    sleep 5
+    sudo docker-compose up -d
+    sleep 180
+    sudo kill -s SIGINT $(sudo docker ps -a -q)
+    sudo service docker restart
 done
